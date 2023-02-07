d49c8a622cd1b1dae6c50153b0bd9d9d17981acf
MrFogg866 <philiplynch866@gmail.com>
2023-02-07 21:11:45 +0000

    update database setting

--- project/settings.py
+++ project/settings.py
@@ -76,12 +76,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
