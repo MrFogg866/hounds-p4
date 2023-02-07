a195cfd9509711c6c2813e2f35228a743e801924
MrFogg866 <philiplynch866@gmail.com>
2023-02-07 22:39:07 +0000

    update sk

--- project/settings.py
+++ project/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SK', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
